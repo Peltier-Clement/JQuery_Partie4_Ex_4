4b02a3a030420e0b304230bee426623ee42ba31d
nisted <clement.p10@laposte.net>
2018-10-24 14:48:15 +0200

    premier depot

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function(){
+  // $('#lastName').val();
+  // $('#firstName').val();
+  // $('#dateOfBirthday').val();
+  // $('#placeOfBirthday').val();
+  // $('#job').val();
+  // $('#society').val();
+  // $('#button').click(function(){
+  //   alert('#lastName' + '#firstName' + 'né le')
+    // alert('Prénom' + 'Nom', + 'né le' + 'dateOfBirthday' + 'à' + 'placeOfBirthday,' + 'actuellement' + 'job' + 'à' + 'society');
+    $('#nom').val();
+    $('#prénom').val();
+    $('#dateDeNaissance').val();
+    $('#lieuDeNaissance').val();
+    $('#emploi').val();
+    $('#société').val();
+    $('#button').click(function(){
+      alert(nom + prénom + 'né le');
+  });
+});
